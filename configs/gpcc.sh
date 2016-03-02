33ec634acae9290505ec485839fdfb48198bbeab
Derek Comingore <dcomingore@pivotal.io>
2016-03-02 13:06:40 -0600

    Update gpcc.sh

--- configs/gpcc.sh
+++ configs/gpcc.sh
@@ -5,5 +5,16 @@ export MASTER_DATA_DIRECTORY=/gpdata/master/gpseg-1
 source /usr/local/greenplum-db/greenplum_path.sh
 source /usr/local/greenplum-cc-web/gpcc_path.sh
 gpstart -a
-#/usr/local/greenplum-cc-web-2.0.0-build-32/bin/gpcmdr --setup --config_file /tmp/gpcmdr.conf
+
+# Detect/Create GPCC Objects on Start
+result=`psql -d gpadmin -t -c "select count(*) from (select 1 from pg_roles where rolname='gpmon')z"`
+if [ $result == 0 ]; then
+  echo "Setting up GPCC"
+  echo 'pivotal\npivotal\n'|createuser -s -l gpmon
+  createdb gpperfmon
+  source /usr/local/greenplum-cc-web/gpcc_path.sh 
+  /usr/local/greenplum-cc-web-2.0.0-build-32/bin/gpcmdr --setup --config_file /tmp/gpcmdr.conf
+fi
+
+# Start up GPCC Instance
 /usr/local/greenplum-cc-web-2.0.0-build-32/bin/gpcmdr --start gpdb_docker
